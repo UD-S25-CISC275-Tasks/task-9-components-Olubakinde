926bc7ff9cf2393588afe1b35b047de5739226ce
Tito Olubakinde <tolubaki@udel.edu>
2025-03-19 21:02:08 -0400

    Update DoubleHalf.tsx

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,20 +1,8 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-/**
- *
- * state cannot be defined outside of where it is being used thats whhy we defind it in here.
- *
- * we can delete that file (DoubleHalfState.tsx).
- *
- */
-
 export function DoubleHalf(): React.JSX.Element {
-    //define state here because state canno be called outside of a component.
     const [dhValue, setDhValue] = useState<number>(10);
-
-    //moved the function inside because these are serving as mini function that work with sate defined above so there is no point in having having outsoide as seperate function as that is not right.
-
     return (
         <div>
             <h3>Double Half</h3>
